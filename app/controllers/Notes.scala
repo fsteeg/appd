247a1298f7f93c941aaa755d8df7664f47ce058d
Fabian Steeg <fsteeg@gmail.com>
2013-09-29 22:02:43 +0200

    Basic processing logic for `/notes` (local Textile files to RSS)

--- app/controllers/Notes.scala
+++ app/controllers/Notes.scala
@@ -2,32 +2,62 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import java.io.File
+import scala.io.Source
+import org.clapper.markwrap.MarkWrap
+import org.clapper.markwrap.MarkupType
+import java.text.SimpleDateFormat
+import java.text.DateFormat
+import java.util.Locale
 
 object Notes extends Controller {
 
   def index = Action {
-    val rss =
-      <rss version="2.0">
-        <channel>
-          <title>Notes RSS feed</title>
-          <description>Feed description</description>
-          <link>http://www.example.com/rss</link>
-          <lastBuildDate>Mon, 05 Oct 2012 11:12:55 =0100 </lastBuildDate>
-          <pubDate>Tue, 06 Oct 2012 09:00:00 +0100</pubDate>
-          {
-            for (title <- List("foo", "bar", "baz")) yield {
-              <item>
-                <title>{ title }</title>
-                <description>Item description</description>
-                <link>http://www.example.com/item</link>
-                <guid isPermaLink="false">123</guid>
-                <pubDate>Tue, 06 Oct 2012 13:00:00 +0100</pubDate>
-              </item>
-            }
+    Ok(notesAsRss("data/notes")).as("application/rss+xml")
+  }
+
+  def notesAsRss(root: String) = {
+    val data = (loadAllFiles(root) sortBy (_._1))(Ordering[String].reverse)
+    Logger.info("data: " + data.toList)
+    rssFor(data).toString
+  }
+
+  def loadAllFiles(root: String) = {
+    (for (file <- new File(root).listFiles; src = Source.fromFile(file))
+      yield (file.getName().split("\\.")(0), parseToHtml(src.mkString)))
+  }
+
+  def parseToHtml(source: String) =
+    MarkWrap.parserFor(MarkupType.Textile).parseToHTML(source)
+
+  def rssFor(data: Seq[(String, String)]) = {
+    val latestRssTime = parseToRssTime(data.head._1)
+    <rss version="2.0">
+      <channel>
+        <title>Notes RSS feed</title>
+        <description>Feed description</description>
+        <link>http://www.example.com/rss</link>
+        <lastBuildDate>{ latestRssTime }</lastBuildDate>
+        <pubDate>{ latestRssTime }</pubDate>
+        {
+          for ((time, text) <- data; rssTime = parseToRssTime(time)) yield {
+            <item>
+              <title>{ rssTime }</title>
+              <description>{ text }</description>
+              <link>http://www.example.com/item</link>
+              <guid isPermaLink="false">{ rssTime }</guid>
+              <pubDate>{ rssTime }</pubDate>
+            </item>
           }
-        </channel>
-      </rss>
-    Ok(rss).as("application/rss+xml")
+        }
+      </channel>
+    </rss>
+  }
+
+  def parseToRssTime(time: String) = {
+    val inputFormat = new SimpleDateFormat("yyyy-mm-dd")
+    val outputFormat = new SimpleDateFormat("E, dd MMM yyyy HH:mm:ss Z")
+    outputFormat.format(inputFormat.parse(time));
   }
 
 }
--- build.sbt
+++ build.sbt
@@ -5,7 +5,8 @@ version := "1.0-SNAPSHOT"
 libraryDependencies ++= Seq(
   jdbc,
   anorm,
-  cache
+  cache,
+  "org.clapper" % "markwrap_2.10" % "1.0.1"
 )     
 
 play.Project.playScalaSettings
