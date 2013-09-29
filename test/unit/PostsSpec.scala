b990cedc03b46718e39a663aaf9567ceb0121017
Fabian Steeg <fsteeg@gmail.com>
2013-09-29 22:13:26 +0200

    Reuse `/notes` processing for `/posts`

--- test/unit/PostsSpec.scala
+++ test/unit/PostsSpec.scala
@@ -5,6 +5,8 @@ import org.specs2.runner._
 import org.junit.runner._
 import play.api.test._
 import play.api.test.Helpers._
+import controllers.Notes
+import scala.xml.XML
 
 /**
  * Add your spec here.
@@ -16,12 +18,13 @@ class PostsSpec extends Specification {
 
   "Posts" should {
 
-    "render the index page" in new WithApplication {
+    "return an RSS feed, sorted from latest to oldest" in new WithApplication {
       val home = route(FakeRequest(GET, "/posts")).get
-
       status(home) must equalTo(OK)
       contentType(home) must beSome.which(_ == "application/rss+xml")
-      contentAsString(home) must contain("Posts RSS")
+      contentAsString(home) must equalTo(Notes.notesAsRss("data/posts"))
+      (XML.loadString(contentAsString(home)) \\ "pubDate")(0).text must
+        equalTo("Mi, 30 Jan 2013 00:09:00 +0100")
     }
   }
 }
